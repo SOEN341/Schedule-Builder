e47886976e8c73941209901472b67be150854e49
Sean Marcoux <seanmarcoux@hotmail.com>
2016-03-22 23:50:02 -0400

    Created ServerBridge Class
    This will be used to mock and create the real implementation for the
    server calls

---
+++ laravel/public/Components/ServerBridge.js
@@ -0,0 +1,30 @@
+var mockServerBridge = {
+	getCourses: function() {
+		return [
+			{
+				name: 'Object Oriented Programming 1',
+				code: 'COMP 248'
+			},
+			{
+				name: 'Object Oriented Programming 2',
+				code: 'COMP 249'
+			}
+		]
+	},
+	
+	getSections: function(course) {
+		
+	},
+	
+	getNeededCourses: function() {
+		
+	},
+	
+	getTakenCourses: function() {
+		
+	}
+};
+
+var realServerBridge = {
+	
+};
