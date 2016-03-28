1b78f1578122c9ba33f84b29383a305cf76c1017
Sean Marcoux <seanmarcoux@hotmail.com>
2016-03-28 02:36:36 -0400

    Courses on the Preferences Page can now be edited
    If you click the new edit icon, you will get the new edit dialog for
    editing courses. It edits as well as updating cookies and updating the
    DB info

--- laravel/public/Components/Preferences.js
+++ laravel/public/Components/Preferences.js
@@ -3,6 +3,8 @@ var PreferencesPage = React.createClass({
 		var cookies=this.loadCookies();
 		return {
 			classDialogOpen: false,
+			editingDialogOpen: false,
+			editingIndex:0,
 			dialogMode: 1,
 			neededCourses: cookies.needed,
 			takenCourses: cookies.taken,
@@ -14,11 +16,22 @@ var PreferencesPage = React.createClass({
 	},
 
 	render: function() {
+		var editingCourse={};
+		if(this.state.dialogMode==1&&this.state.takenCourses.length>0) {
+			console.log('edit taken course ' + this.state.editingIndex);
+			editingCourse=this.state.takenCourses[this.state.editingIndex];
+		}
+		else if(this.state.neededCourses.length>0) {
+			console.log('edit needed course ' + this.state.editingIndex);
+			editingCourse=this.state.neededCourses[this.state.editingIndex];
+		}
+		
 		return (
 			<div>
 				{this.state.classDialogOpen? <ClassDialog mode={this.state.dialogMode} close={this.closeClassDialog} addNeededCourse={this.addNeededCourse} addTakenCourse={this.addTakenCourse} courses={this.state.courses}/>: null}
+				{this.state.editingDialogOpen? <EditingDialog mode={this.state.dialogMode} close={this.closeEditingDialog} courses={this.state.courses} course={editingCourse} edit={this.editCourse}/>: null}
 				<Preferences courseLoad={this.state.courseLoad} day={this.state.day} time={this.state.time}/>
-				<Classes binder={this} openDialog={this.openClassDialog} takenCourses={this.state.takenCourses} neededCourses={this.state.neededCourses} removeTakenCourse={this.removeTakenCourse} removeNeededCourse={this.removeNeededCourse}/>
+				<Classes binder={this} openDialog={this.openClassDialog} takenCourses={this.state.takenCourses} neededCourses={this.state.neededCourses} removeTakenCourse={this.removeTakenCourse} removeNeededCourse={this.removeNeededCourse} editNeededCourse={this.startEditNeededCourse} editTakenCourse={this.startEditTakenCourse}/>
 				<br/>
 				<div style={{textAlign:'center'}}><RBS.Button onClick={this.generateSchedule} bsStyle='primary'>Build Schedule</RBS.Button></div>
 			</div>
@@ -73,6 +86,20 @@ var PreferencesPage = React.createClass({
 	closeClassDialog: function() {
 		this.setState({
 			classDialogOpen: false
+		})
+	},
+	
+	openEditingDialog: function(mode, index) {
+		this.setState({
+			editingDialogOpen: true,
+			dialogMode: mode,
+			editingIndex: index
+		})
+	},
+
+	closeEditingDialog: function() {
+		this.setState({
+			editingDialogOpen: false
 		})
 	},
 	
@@ -146,6 +173,59 @@ var PreferencesPage = React.createClass({
 		}
 	},
 	
+	startEditNeededCourse: function(number) {
+		var courses = React.addons.update(this.state.neededCourses, {});
+		var index=-1;
+		for(var i=0; i<courses.length; i++) {
+			if(courses[i].number==number)
+			{
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1) {
+			this.openEditingDialog(0, index);
+		}
+	},
+	
+	startEditTakenCourse: function(number) {
+		var courses = React.addons.update(this.state.takenCourses, {});
+		var index=-1;
+		for(var i=0; i<courses.length; i++) {
+			if(courses[i].number==number)
+			{
+				index=i;
+				break;
+			}
+		}
+		if(index!=-1) {
+			this.openEditingDialog(1, index)
+		}
+	},
+	
+	editCourse: function(course) {
+		if(this.state.mode==1) {
+			var courses = React.addons.update(this.state.takenCourses, {});
+			courses[this.state.editingIndex]=course;
+			this.setState({
+				takenCourses: courses,
+				editingDialogOpen: false
+			});
+			cookieManager.addCookie('taken', JSON.stringify(courses), 7);
+			serverBridge.editTakenCourses(courses);
+		}
+		else {
+			var courses = React.addons.update(this.state.neededCourses, {});
+			courses[this.state.editingIndex]=course;
+			this.setState({
+				neededCourses: courses,
+				editingDialogOpen: false
+			});
+			cookieManager.addCookie('needed', JSON.stringify(courses), 7);
+			serverBridge.editNeededCourses(courses);
+		}
+	},
+	
 	onClassesChange: function(value) {
 		this.setState({
 			classes: value
@@ -194,7 +274,7 @@ var ClassDialog = React.createClass({
 					</RBS.Grid>
 				</RBS.Modal.Body>
 				<RBS.Modal.Footer>
-					<RBS.Button onClick={this.addCourse} bsStyle='primary'>Add Class</RBS.Button>
+					<RBS.Button onClick={this.addCourse} bsStyle='primary'>Add Course</RBS.Button>
 				</RBS.Modal.Footer>
 			</RBS.Modal>
 		)
@@ -229,6 +309,71 @@ var ClassDialog = React.createClass({
 			this.props.addTakenCourse(course);
 		else
 			this.props.addNeededCourse(course);
+		this.props.close();
+	}
+});
+
+var EditingDialog = React.createClass({
+	getInitialState: function() {
+		return {
+			name: this.props.course.name,
+			number: this.props.course.number
+		}
+	},
+	
+	render:function() {
+		var type='Needed';
+		if(this.props.mode==1) {
+			type='Taken';
+		}
+		return (
+			<RBS.Modal show={true} onHide={this.props.close}>
+				<RBS.Modal.Header closeButton>
+					<RBS.Modal.Title>Edit {type} Course</RBS.Modal.Title>
+				</RBS.Modal.Header>
+				<RBS.Modal.Body>
+					<RBS.Grid fluid={true}>
+						<RBS.Row>
+							<TypeaheadInput label='Course Number' onChange={this.numberChange} value={this.state.number} data={this.props.courses} type='number' key={1}/>
+						</RBS.Row>
+						<RBS.Row>
+							<InputElement label='Course Name' onChange={this.nameChange} value={this.state.name} data={this.props.courses} key={2}/>
+						</RBS.Row>
+					</RBS.Grid>
+				</RBS.Modal.Body>
+				<RBS.Modal.Footer>
+					<RBS.Button onClick={this.editCourse} bsStyle='primary'>Edit Course</RBS.Button>
+				</RBS.Modal.Footer>
+			</RBS.Modal>
+		)
+	},
+	
+	numberChange: function(value) {
+		if(value.length==8) {
+			for(var i=0; i<this.props.courses.length; i++) {
+				if(this.props.courses[i].number==value) {
+					this.nameChange(this.props.courses[i].name);
+					break;
+				}
+			}
+		}
+		this.setState({
+			number: value
+		});
+	},
+	
+	nameChange: function(value) {
+		this.setState({
+			name: value
+		});
+	},
+	
+	editCourse: function() {
+		var course = {
+			name: this.state.name,
+			number: this.state.number
+		};
+		this.props.edit(course);
 		this.props.close();
 	}
 });
@@ -290,8 +435,8 @@ var Classes = React.createClass({
 						<RBS.Col md={2}><RBS.Button>Generate class list</RBS.Button></RBS.Col>
 					</RBS.Row>
 				</RBS.Grid>
-				<TakenClasses openDialog={this.props.openDialog} courses={this.props.takenCourses} remove={this.props.removeTakenCourse} binder={this.props.binder}/>
-				<NeededClasses openDialog={this.props.openDialog} courses={this.props.neededCourses} remove={this.props.removeNeededCourse} binder={this.props.binder}/>
+				<TakenClasses openDialog={this.props.openDialog} courses={this.props.takenCourses} remove={this.props.removeTakenCourse} binder={this.props.binder} editCourse={this.props.editTakenCourse}/>
+				<NeededClasses openDialog={this.props.openDialog} courses={this.props.neededCourses} remove={this.props.removeNeededCourse} binder={this.props.binder} editCourse={this.props.editNeededCourse}/>
 			</div>
 		)
 	},
@@ -313,7 +458,7 @@ var TakenClasses = React.createClass({
 							<th colSpan={3}>Classes Taken</th>
 						</tr>
 					</thead>
-					<CourseList courses={this.props.courses} remove={this.props.remove} binder={this.props.binder}/>
+					<CourseList courses={this.props.courses} remove={this.props.remove} binder={this.props.binder} editCourse={this.props.editCourse}/>
 				</RBS.Table>
 				<div style={{textAlign:'center'}}><RBS.Button onClick={this.openDialog}>Add Class</RBS.Button></div>
 				<br/>
@@ -336,7 +481,7 @@ var NeededClasses = React.createClass({
 							<th colSpan={3}>Classes Needed</th>
 						</tr>
 					</thead>
-					<CourseList courses={this.props.courses} remove={this.props.remove} binder={this.props.binder}/>
+					<CourseList courses={this.props.courses} remove={this.props.remove} binder={this.props.binder} editCourse={this.props.editCourse}/>
 				</RBS.Table>
 				<div style={{textAlign:'center'}}><RBS.Button onClick={this.openDialog}>Add Class</RBS.Button></div>
 			</div>
@@ -363,7 +508,7 @@ var CourseList = React.createClass({
 				</tr>
 				{this.props.courses.map(function(course) {
 					return (
-						<Course key={this.keys++} name={course.name} number={course.number} remove={this.props.remove.bind(this.props.binder, course.number)}/>
+						<Course key={this.keys++} name={course.name} number={course.number} remove={this.props.remove.bind(this.props.binder, course.number)} edit={this.props.editCourse.bind(this.props.binder, course.number)}/>
 					)
 				}, this)}
 			</tbody>
@@ -374,7 +519,10 @@ var CourseList = React.createClass({
 var Course = React.createClass({
 	render: function() {
 		return (
-			<tr><td>{this.props.name}</td><td>{this.props.number}</td><td><img onClick={this.props.remove} src="Images/delete.png" title="Remove Course" style={{height: '15px', width: '15px'}}/></td></tr>
+			<tr><td>{this.props.name}</td>
+			<td>{this.props.number}</td>
+			<td><img onClick={this.props.edit} src="Images/edit.png" title="Edit Course" style={{height: '15px', width: '15px'}}/>&nbsp;&nbsp;
+			<img onClick={this.props.remove} src="Images/delete.png" title="Remove Course" style={{height: '15px', width: '15px'}}/></td></tr>
 		)
 	}
 });
