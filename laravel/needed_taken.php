9bcce8036113791297ed9acf21e0676995ccf5f4
Bruce <edouard.12@live.ca>
2016-03-29 02:35:33 -0400

    added a prototype for cneeded

--- laravel/needed_taken.php
+++ laravel/needed_taken.php
@@ -7,8 +7,17 @@ class cneeded {
 		$this->number=$number;
 		$this->name=$name;
 	}
+}
 
 
+class ctaken {
+	public $name="";
+	public $number="";
+			
+	function __construct($name,$number){
+		$this->number=$number;
+		$this->name=$name;
+	}
 }
 
 ?>
--- laravel/resources/views/editcneeded.blade.php
+++ laravel/resources/views/editcneeded.blade.php
@@ -5,8 +5,8 @@
 // $number=$_POST['number'];//username
 
 $old='jason';//username
-$name='OBJ-ORIENTED PROGRAMMING I';//username
-$number='249';//username
+$name='OBJ-ORIENTED PROGRAMMING II';//username
+$number='250';//username
 
 
 
@@ -14,50 +14,33 @@ require_once('../mysqli_connect.php'); // defining and connecting to the databas
 require_once('../needed_taken.php');
 
 
-$updatedCNeeded=serialize(array(new cneeded($name,$number)));
-
-echo $updatedCNeeded;
-
-
-//CoursesDones 	CoursesRem 	CLoad 	dayOff 	pTime 
- $sql = "UPDATE `users` SET `CoursesDones`='$updatedCNeeded' WHERE username='$old'";
-
-$response= mysqli_query($dbc,$sql);
-
-
-
-
-
-
 $sql= "SELECT * FROM users where username='$old'";
 $response= mysqli_query($dbc,$sql);
+$unserialized=unserialize(mysqli_fetch_array($response)['CoursesDones']);
+
+array_push($unserialized, new cneeded($name,$number));
+
+$entry=serialize($unserialized);
+
+//CoursesDones 	CoursesRem 	CLoad 	dayOff 	pTime 
+ $sql = "UPDATE `users` SET `CoursesDones`='$entry' WHERE username='$old'";
+
+$response= mysqli_query($dbc,$sql);
+
+echo json_encode(array("success"=>"true","username"=>"$old","Cnumber"=>"$number"));  
 
 
 
-foreach (unserialize(mysqli_fetch_array($response)['CoursesDones']) as $arr) {
-    	echo nl2br( $arr->name . ",  " .  $arr->number );
-    }
+
+// $sql= "SELECT * FROM users where username='$old'";
+// $response= mysqli_query($dbc,$sql);
 
 
 
+// foreach (unserialize(mysqli_fetch_array($response)['CoursesDones']) as $arr) {
+//     	echo nl2br( $arr->name . ",  " .  $arr->number );
+//     }
 
-
-
-
-
-// $query ="SELECT username FROM users WHERE email='$new' "; // search for the user
-// // with these credentials                          
-
-// $response= mysqli_query($dbc,$query); 
-
-
-// if(mysqli_num_rows($response) <= 0){ 
-//  $sql = "UPDATE `users` SET `email`='$new' WHERE username='$old'";
-//  $response= mysqli_query($dbc,$sql); 
-//  echo json_encode(array("success"=>"true","username"=>"$old","email"=>"$new"));             
-// } else {
-//   echo json_encode(array("success"=>"false","username"=>"$old","email"=>"$new"));  
-// }
 mysqli_close($dbc);
    
 
