598fa7f8fe7d62506ccc3fd1443f638951eb357a
sihx <telescope72@hotmail.com>
2016-03-30 14:37:21 -0400

    Added the basic science electives

--- laravel/Updated341.sql
+++ laravel/Updated341.sql
@@ -98,7 +98,29 @@ INSERT INTO `courses` (`courseId`, `courseCode`, `semester`, `description`, `nam
 (55, 'ENGR 301','Winter','Introduction to project delivery systems. Principles of project management; role and activity of a manager.','Engineering Management Principles and Economics',3),
 (56, 'ENGR 392','Summer','Social history of technology and of science including the industrial revolution and modern times.','Impact of Technology on Society',3),
 (57, 'ENGR 392','Fall','Social history of technology and of science including the industrial revolution and modern times.','Impact of Technology on Society',3),
-(58, 'ENGR 392','Winter','Social history of technology and of science including the industrial revolution and modern times.','Impact of Technology on Society',3);
+(58, 'ENGR 392','Winter','Social history of technology and of science including the industrial revolution and modern times.','Impact of Technology on Society',3),
+(59, 'BIOL 206','Winter','A survey of classical and contemporary developments in the study of heredity, with particular attention to human examples.','Elementary Genetics',3),
+(60, 'BIOL 261','Fall','Basic genetic principles, including mechanisms of meiosis and mitosis, Mendelian genetics, recombination, gene mapping, and chromosome rearrangements','Molecular and General Genetics',3),
+(61, 'BIOL 261','Winter','Basic genetic principles, including mechanisms of meiosis and mitosis, Mendelian genetics, recombination, gene mapping, and chromosome rearrangements','Molecular and General Genetics',3),
+(62, 'CHEM 217','Fall','Precipitation methods and solubility products; activity, chemical equilibria and titration curves of neutralization and complexation systems','Introductory Analytical Chemistry I',3),
+(63, 'CHEM 221','Fall','Basic aspects of orbitals and their role in covalent bonding; delocalization of electrons.','Introductory Organic Chemistry I',3),
+(64, 'CHEM 221','Winter','Basic aspects of orbitals and their role in covalent bonding; delocalization of electrons.','Introductory Organic Chemistry I',3),
+(65, 'CIVI 231','Fall','Basic principles of physical and structural geology with emphasis on topics related to civil engineering','Geology for Civil Engineers',3),
+(66, 'ELEC 321','Winter','Fundamentals underlying optical and electronic devices. The structure and growth of crystals. ','Introduction to Semiconductor Materials and Devices',3.5),
+(67, 'ENGR 242','Fall','Resultant of force systems; equilibrium of particles and rigid bodies; distributed forces; statically determinate systems','Statics',3),
+(68, 'ENGR 242','Winter','Resultant of force systems; equilibrium of particles and rigid bodies; distributed forces; statically determinate systems','Statics',3),
+(69, 'ENGR 243','Summer','Kinematics of a particle and rigid body; forces and accelerations; work and energy; impulse and momentum.','Dynamics',3),
+(70, 'ENGR 243','Fall','Kinematics of a particle and rigid body; forces and accelerations; work and energy; impulse and momentum.','Dynamics',3),
+(71, 'ENGR 243','Winter','Kinematics of a particle and rigid body; forces and accelerations; work and energy; impulse and momentum.','Dynamics',3),
+(72, 'ENGR 251','Summer','Basic principles of thermodynamics and their application to various systems composed of pure substances and their homogeneous non-reactive mixtures.','Thermodynamics I',3),
+(73, 'ENGR 251','Fall','Basic principles of thermodynamics and their application to various systems composed of pure substances and their homogeneous non-reactive mixtures.','Thermodynamics I',3),
+(74, 'ENGR 251','Winter','Basic principles of thermodynamics and their application to various systems composed of pure substances and their homogeneous non-reactive mixtures.','Thermodynamics I',3),
+(75, 'ENGR 361','Summer','Basic concepts and principles of fluid mechanics. Classification of fluid flow. Hydrostatic forces on plane and curved surfaces, buoyancy and stability, fluids in rigid body motion.','Fluid Mechanics I',3),
+(76, 'ENGR 361','Fall','Basic concepts and principles of fluid mechanics. Classification of fluid flow. Hydrostatic forces on plane and curved surfaces, buoyancy and stability, fluids in rigid body motion.','Fluid Mechanics I',3),
+(77, 'ENGR 361','Winter','Basic concepts and principles of fluid mechanics. Classification of fluid flow. Hydrostatic forces on plane and curved surfaces, buoyancy and stability, fluids in rigid body motion.','Fluid Mechanics I',3),
+(78, 'MECH 221','Fall','Relationships between properties and internal structure, atomic bonding; molecular, crystalline and amorphous structures.','Materials Science',3),
+(79, 'MECH 221','Winter','Relationships between properties and internal structure, atomic bonding; molecular, crystalline and amorphous structures.','Materials Science',3),
+(80, 'PHYS 252','Winter','Wave equation, phasors, EM waves, linear, circular and elliptical polarization, polariscope, Malus law, dichroism.','Optics',3);
 
 -- --------------------------------------------------------
 
