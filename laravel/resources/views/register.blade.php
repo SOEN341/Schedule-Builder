d6310bed598f12bc73135ce4c05b4b3ebd9b4acc
sihx <telescope72@hotmail.com>
2016-04-01 01:39:41 -0400

    Encryption is setup

--- laravel/resources/views/register.blade.php
+++ laravel/resources/views/register.blade.php
@@ -2,12 +2,12 @@
 require_once('../mysqli_connect.php');
 $username=$_POST['username'];
 $password=$_POST['password'];
+$encryptedPassword = Hash::make('$password');
 $email=$_POST['email'];
 
 // $username='SprinkKing';
 // $password='pass1';
 // $email='email@email.com';
-
 
 $query ="SELECT username FROM users WHERE username='$username' ";
 // Check if the username exist already
@@ -15,8 +15,13 @@ $query ="SELECT username FROM users WHERE username='$username' ";
 $response= mysqli_query($dbc,$query);
 
  if(mysqli_num_rows($response) <= 0){
-    //$sql = "INSERT INTO `341`.`users` (`username`, `email`, `userType`, `password`, `CoursesDones`, `CoursesRem`, `CLoad`, `dayOff`, `pTime`) VALUES ('$username', '$email', '0', '$password', 'cdone', 'crem', '4', 'Monday', 'Afternoons');";
-    $sql = "INSERT INTO users (username, email, userType, password, CoursesDones, CoursesRem, CLoad, dayOff, pTime) VALUES ('$username', '$email', FALSE ,'$password', 'cdone', 'crem', '4', 'Monday', 'Afternoons')";
+    //$sql = "INSERT INTO `341db`.`users` (`username`, `email`, `userType`, `password`, `CoursesDones`, `CoursesRem`, `CLoad`, `dayOff`, `pTime`) VALUES ('$username', '$email', '0', '$password', 'cdone', 'crem', '4', 'Monday', 'Afternoons');";
+
+     if(Hash::needsRehash($encryptedPassword)){
+         $encryptedPassword = Hash::make('$password');
+     }
+
+     $sql = "INSERT INTO users (username, email, userType, password, CoursesDones, CoursesRem, CLoad, dayOff, pTime) VALUES ('$username', '$email', FALSE ,'$encryptedPassword', 'cdone', 'crem', '4', 'Monday', 'Afternoons')";
 
         if (mysqli_query($dbc, $sql)) {
             echo json_encode(array("success"=>"true","username"=>"$username","password"=>"$password"));
