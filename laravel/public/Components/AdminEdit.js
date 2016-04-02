6dd794ca697410debbbfd47fea902252815e0ac9
Gabriele Bavaro <gabriele.bavaro@bell.net>
2016-04-02 13:22:48 -0400

    Update

--- laravel/public/Components/AdminEdit.js
+++ laravel/public/Components/AdminEdit.js
@@ -2,6 +2,7 @@ var AdminPage = React.createClass({
 	getInitialState: function() {
 		return {
 			courses: []
+			this.getCookies().JSON.parse
 		}
 	},
 	
@@ -50,7 +51,31 @@ var AdminCourse = React.createClass({
 			<td>{this.props.course.beginTime}</td>
 			<td>{this.props.course.endTime}</td>
 			<td>{this.props.course.classroom}</td>
-			<td><img src="Images/delete.png" title="Remove Course" style={{height: '15px', width: '15px'}}/></td></tr>
+			<td><img onClick={this.props.editCourse} src="Images/edit.png" title="Edit Course" style={{height: '15px', width: '15px'}}/>&nbsp;&nbsp;</td></tr>
+		)
+	}
+});
+
+var CourseList = React.createClass({
+	componentWillMount: function() {
+		this.keys=0;
+	},
+	
+	render: function() {
+		return (
+			<tbody>
+				<tr>
+					<td style={{width: '40%'}}>Class Name</td>
+					<td style={{width: '40%'}}>Course Number</td>
+					<td style={{width: '20%'}}></td>
+				</tr>
+				{this.props.courses.map(function(course) {
+					return (
+						<Course key={this.keys++} name={course.name} number={course.number} remove={this.props.remove.bind(this.props.binder, course.number)} 
+						editCourse={this.props.editCourse.bind(this.props.binder, course.number)}/>
+					)
+				}, this)}
+			</tbody>
 		)
 	}
 });
