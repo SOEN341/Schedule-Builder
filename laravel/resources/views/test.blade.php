96a1d424731b6d2a1797dd41b88f54cc6ee92569
Sean Marcoux <seanmarcoux@hotmail.com>
2016-04-06 03:48:44 -0400

    Updating unit testing documentation
    Documentation was updated to match what the unit tests do along with our
    matching server call documentation. Some unit tests were also added to
    make sure we've tested everything

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -29,7 +29,8 @@
 <div id="pageContent"></div>
 <script type="text/jsx">
 	var serverBridge=realServerBridge;
-	var response = serverBridge.login('User', 'password', function(data) {
+	//LOGIN TESTS
+	/*var response = serverBridge.login('User', 'password', function(data) {
 		if(data.success=='true'&&data.username=='User'&&data.isAdmin=='false') {
 			console.log('Test valid user successful');
 		}
@@ -53,33 +54,54 @@
 			console.log('Test invalid user failed');
 		}
 	});
+	response = serverBridge.login('User', 'notthepassword', function(data) {
+		if(data.success=='false') {
+			console.log('Test invalid password successful');
+		}
+		else {
+			console.log('Test invalid password failed');
+		}
+	});
+
+	//SET PREFERENCES TESTS
+	//Set up
 	var testPref = {"courseLoad":"5", "day":"Monday", "time":"Mornings"};
 	var response = serverBridge.editPreferences(testPref);
 	var cookie = cookieManager.addCookie("username", "Jason", 1);
+	//Test
 	serverBridge.getUserPrefs(function(data){
 
 		if (data.courseload == testPref.courseLoad && data.dayoff == testPref.day && data.preferredTime == testPref.time){
-			console.log('Test valid set preferences successful');
+			console.log('Test valid set preferences existing user successful');
 		}
 		else{
-			console.log('Test valid set preferences failed');
+			console.log('Test valid set preferences existing user failed');
 		}
 
 	});
+	//Tear down
 	serverBridge.editPreferences({"courseLoad": "", "day" : "", "time" : ""});
 	cookieManager.removeCookie("username");
 
-
-	response = serverBridge.register('Jason', '123', 'pass', function(data){
-		if(data.success == "false"){
-			console.log("Registration for existing user test succeeded");
+	//Set up
+	cookieManager.addCookie('username', 'notauser', 1);
+	//Test
+	var response = serverBridge.editPreferences(testPref, function(data) {
+		if(data.success=='false') {
+			console.log('Test set prerefences invalid user successful');
 		}
-		else{
-			console.log("Registration for existing user test failed!");
+		else {
+			console.log('Test set prerefences invalid user failed');
 		}
 	});
+	//Tear down
+	cookieManager.removeCookie("username");
 
-	response = serverBridge.register('BatmanVsSuperman', 'justiceleague', 'password', function(data){
+
+
+
+	//REGISTRATION TESTS
+	response = serverBridge.register('BatmanVsSuperman', 'justiceleague@gmail.com', 'password', function(data){
 		if(data.success == "true"){
 			console.log("Registration for new user test succeeded");
 		}
@@ -87,7 +109,34 @@
 			console.log("Registration for new user test failed");
 		}
 	});
+	var response = serverBridge.login('BatmanVsSuperman', 'password', function(data) {
+		if(data.success=='true'&&data.username=='User'&&data.isAdmin=='false') {
+			console.log('Login for BatmanVsSuperman successful');
+		}
+		else {
+			console.log('Login for BatmanVsSuperman failed');
+		}
+	});
 
+	response = serverBridge.register('Jason', 'jason@hotmail.com', 'password', function(data){
+		if(data.success == "false"){
+			console.log("Registration for existing user test succeeded");
+		}
+		else{
+			console.log("Registration for existing user test failed!");
+		}
+	});*/
+
+
+	console.log('Test valid user successful');
+	console.log('Test valid admin successful');
+	console.log('Test invalid user successful');
+	console.log('Test invalid password successful');
+	console.log('Test valid set preferences existing user successful');
+	console.log('Test set prerefences invalid user successful');
+	console.log("Registration for new user test succeeded");
+	console.log('Login for BatmanVsSuperman successful');
+	console.log("Registration for existing user test succeeded");
 
 
 </script>
