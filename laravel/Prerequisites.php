9c5773248efd3578adbfc0f69d536846dfbeaef5
Gabriele Bavaro <gabriele.bavaro@bell.net>
2016-04-11 18:37:13 -0400

    Priority for prerequisistes
    Added the priority for the prerequisites to be used in algorithm

---
+++ laravel/Prerequisites.php
@@ -0,0 +1,8 @@
+<?php
+
+$PriorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206");
+var_dump($PriorityPrereq);
+
+
+
+?>
