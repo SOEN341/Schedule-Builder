170a06ed1aca7fe35ebbdde5ec059248eefdacb9
Gabriele Bavaro <gabriele.bavaro@bell.net>
2016-04-11 19:27:31 -0400

    Update

--- laravel/Prerequisites.php
+++ laravel/Prerequisites.php
@@ -1,6 +1,7 @@
 <?php
 
-$PriorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206");
+$PriorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206", "SOEN 228", "MATH 203", "MATH 204",
+   "CHEM 204", "PHYS 204", "PHYS 205", "PHYS 206");
 var_dump($PriorityPrereq);
 
 
