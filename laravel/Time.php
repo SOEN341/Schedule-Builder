5d8157333556cf816b3a9810b15ee6a2dd1abbf5
Gabriele Bavaro <gabriele.bavaro@bell.net>
2016-04-11 21:20:53 -0400

    Partially completed collision checker
    Collision checker for schedule partially completed

--- laravel/Time.php
+++ laravel/Time.php
@@ -1,22 +1,44 @@
 <?php
 class Time {
-    public $Day='Monday';
-    public $StartTime='13:35:00';
-    public $EndTime='14:35:00';
+    public $Day='';
+    public $StartTime='';
+    public $EndTime='';
 
     public $Testday='Monday';
     public $TestStart='13:50:00';
     public $Testend='14:30:00';
 
-    function __Test($NewDay,$NewStart,$NewEnd){
-        if($this->Testdayay == $this->Day && ($this->TestStart > $this->StartTime) && ($this->TestStart < $this->EndTime))
+    function __construct($OldDay,$OldStart,$OldEnd){
+        $this->Day=$OldDay;
+        $this->StartTime=$OldStart;
+        $this->EndTime=$OldEnd;
+    }
+
+    function Test($NewDay,$NewStart,$NewEnd){
+
+        $modifyOne = strtotime($this->StartTime);
+        $modifyTwo = strtotime($this->EndTime);
+        $modifyThree = strtotime($NewStart);
+        $modifyFour = strtotime($NewEnd);
+
+        if($NewDay == $this->Day && ((($modifyThree > $modifyOne) && ($modifyThree < $modifyTwo)) || (($modifyFour > $modifyOne) && ($modifyFour < $modifyTwo)) ||(($modifyThree == $modifyOne) && ($modifyFour == $modifyTwo))))
         {
             echo "The test works";
         }
-        _test('Monday', '13:50:00', '14:30:00');
-    }
-}
 
+        if($NewDay == $this->Day && (($modifyOne == $modifyFour) || ($modifyThree == $modifyTwo)))
+        {
+            echo "The test works";
+        }
+
+        else
+        {
+            echo "Everything is good";
+        }
+
+    }
+
+}
 
 
 ?>
