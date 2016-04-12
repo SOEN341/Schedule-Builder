38c8d35633ff6f6ffc6d09377408f5424451f386
Bruce <edouard.12@live.ca>
2016-04-12 17:45:06 -0400

    tentative fix

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -17,6 +17,8 @@ $coursesRem=Array();
 $coursesInformationDone = Array();
 $coursesInformationRem = Array();
 
+$courseload='';
+
 $query ="SELECT * FROM users where username='$username' ";
 // Check if these credentials are taken
 
@@ -27,6 +29,8 @@ $array= mysqli_fetch_array($response);
 
 //$priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206", "SOEN 228", "MATH 203", "MATH 204","CHEM 204", "PHYS 204", "PHYS 205", "PHYS 206");
 $priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233","SOEN 228");
+
+$courseload=$array['CLoad'];
 
 $decodedDone=json_decode($array['CoursesDones'],true);
 
@@ -45,14 +49,13 @@ foreach ($decodedrem as $key => $key_value) {
 		   }
 }
 
-array_push($coursesDone,"MATH 205","MATH 203","MATH 204"); //hardcoded because not added autamitically when the course list is generated
+array_push($coursesDone,"MATH 205","MATH 203","MATH 204","PHYS 204","PHYS 205","CHEM 203"); //hardcoded because not added autamitically when the course list is generated
 
 //var_dump($priorityPrereq);
 
 //var_dump($coursesDone);
 
 // var_dump($coursesRem);
-
 
 
 $schedule= new Schedule($coursesRem,$coursesDone,4,'Monday','Morning',$priorityPrereq);
@@ -164,8 +167,115 @@ foreach ($courses as $key => $value){
 //var_dump($arrayofcourses);
 
 foreach ($arrayofcourses as $key => $value) {
+	// if (count($schedule->schedule)==$courseload) {
+	// 	break;
+	// }
 	$schedule->addCourse($value);
 }
+
+$remainingList=Array();
+
+foreach ($anot as $key => $value) {
+	array_push($remainingList,$value); //pushing the courses tha the student is going to need because they are important prereqs for the priority list
+}
+
+foreach ($coursesRem as $key => $value) {
+array_push($remainingList,$value);
+}
+
+//var_dump($courses);
+//var_dump(array_unique($remainingList));
+$remaininglist=array_diff($remainingList, $courses);
+var_dump(array_unique($remaininglist));
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//removing the courses from the needed list that the user does not have the preereq for
+
+
+$postCull2=Array();
+$anot2=Array();
+
+//var_dump($courses);
+
+foreach ($remaininglist as $x => $x_value) {
+	//echo "$x_value";
+	$query ="SELECT * FROM courses where coursecode='$x_value' ";
+	// Check if these credentials are taken
+	$response= mysqli_query($dbc,$query);
+
+	$answer= mysqli_fetch_array($response);
+
+	$cid=$answer['courseId'];
+
+	//echo " $cid"  ;
+
+	$query ="SELECT prerequisitesList FROM prerequisites where courseId='$cid' ";
+	// Check if these credentials are taken
+	$response= mysqli_query($dbc,$query);
+
+	//var_dump($response);	
+
+	$answer2= mysqli_fetch_array($response);
+
+	//var_dump($answer2);
+
+	$json=json_decode($answer2['prerequisitesList'],true);
+
+	//$temp=$json['List'];
+
+	//var_dump($json);
+
+	//echo $json['List']; //array to string conversion error
+
+	$temp=$json['List'];
+
+	$fcourse=$x_value;
+
+	//echo "$fcourse";
+
+	//var_dump($temp);
+
+	if(count($temp)===0)
+		array_push($postCull2,$fcourse);
+	else
+	foreach ($temp as $key => $value) {		
+			//echo $value['courseCode'];
+			$val=$value['courseCode'];
+			//echo " needs:" . $val;
+			if(in_array("$val", $coursesDone,true)){
+				//echo " True.  ";
+				array_push($postCull2,$fcourse);
+			} else {
+				//echo "not.    ";
+				array_push($anot2, $fcourse);
+			}
+		
+	}
+
+	}	
+
+//var_dump(array_unique($anot2));
+//var_dump($postCull2);
+$remaininglist=array_unique(array_diff(array_unique($postCull2),array_unique($anot2)));	//list of priotiry courses after the prereq were removed
+
+var_dump($remaininglist);
+
+foreach ($remaininglist as $key => $value) {
+	// if (count($schedule->schedule)==$courseload) {
+	// 	break;
+	// }
+	$schedule->addCourse($value);
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
 
 //var_dump($schedule);
 
