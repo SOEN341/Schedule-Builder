96da44739dcb3d1bd17c271967f8fdc7f8fc7682
sihx <telescope72@hotmail.com>
2016-04-11 22:09:11 -0400

    Update

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -2,7 +2,7 @@
 require_once('../mysqli_connect.php');
 require_once('../preferences');
 //$username=$_POST['username'];
-$username='user17';
+$username='Jason123';
 
 $query ="SELECT * FROM users where username='$username' ";
 // Check if these credentials are taken
@@ -13,6 +13,9 @@ $array= mysqli_fetch_array($response);
 
 $coursesDone=Array();
 $coursesRem=Array();
+$coursesInformationDone = Array();
+$coursesInformationRem = Array();
+
 
 //$priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206", "SOEN 228", "MATH 203", "MATH 204","CHEM 204", "PHYS 204", "PHYS 205", "PHYS 206");
 $priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233","SOEN 228");
@@ -22,6 +25,7 @@ $decodedDone=json_decode($array['CoursesDones'],true);
 foreach ($decodedDone as $key => $key_value) {
 		foreach ($key_value as $x => $x_value) {
 		array_push($coursesDone,$x_value['number']);
+
 		    }
 }
 
@@ -41,8 +45,30 @@ foreach ($decodedrem as $key => $key_value) {
 
 $schedule= new Schedule($coursesRem,$coursesDone,4,'Monday','Morning',$priorityPrereq);
 
-var_dump($schedule);
 
+$courses=  $schedule->courses;
+
+
+foreach ($courses as $key => $value){
+	//$temp = $value['number'];
+	$queryForCoursesDone = mysqli_fetch_array(mysqli_query($dbc,"SELECT * FROM sections WHERE courseCode = '$value' "));
+	$query ="SELECT * FROM sections where courseCode='$value' ";
+	array_push($coursesInformationDone, $queryForCoursesDone);
+
+	//this returns all the sections
+	//create an object $course containing all the sections(lecture, lab, tutorial) for all the courses in $courses
+}
+
+print_r(array_values($coursesInformationDone));
+//print_r(array_values($courses));
+
+//loop through $arrayofcourses
+//schedule.add($courses)
+
+
+
+
+//var_dump($schedule);
 
 // $query ="SELECT * FROM courses where username='$username' ";
 // // Check if these credentials are taken
