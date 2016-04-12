88bc47a63903d8584d1fae7c5cd9091252521e88
Bruce <edouard.12@live.ca>
2016-04-11 20:23:02 -0400

    pushed the test.php blade new version

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -11,15 +11,47 @@ $response= mysqli_query($dbc,$query);
 
 $array= mysqli_fetch_array($response);
 
-echo  $array['CoursesDones'];
+$coursesDone=Array();
+$coursesRem=Array();
 
-$priority=array();
+//$priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206", "SOEN 228", "MATH 203", "MATH 204","CHEM 204", "PHYS 204", "PHYS 205", "PHYS 206");
+$priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233","SOEN 228");
 
-// echo  $array['CoursesRem'];
+$decodedDone=json_decode($array['CoursesDones'],true);
 
-$schedule= new Schedule(4,'Monday','Morning',$priority);
+foreach ($decodedDone as $key => $key_value) {
+		foreach ($key_value as $x => $x_value) {
+		array_push($coursesDone,$x_value['number']);
+		    }
+}
+
+$decodedrem=json_decode($array['CoursesRem'],true);
+
+foreach ($decodedrem as $key => $key_value) {
+		foreach ($key_value as $x => $x_value) {
+		array_push($coursesRem,$x_value['number']);
+		   }
+}
+
+// var_dump($priorityPrereq);
+
+// var_dump($coursesDone);
+
+// var_dump($coursesRem);
+
+$schedule= new Schedule($coursesRem,$coursesDone,4,'Monday','Morning',$priorityPrereq);
 
 var_dump($schedule);
+
+
+// $query ="SELECT * FROM courses where username='$username' ";
+// // Check if these credentials are taken
+
+// $response= mysqli_query($dbc,$query);
+
+// $array= mysqli_fetch_array($response);
+
+
 
 
 
