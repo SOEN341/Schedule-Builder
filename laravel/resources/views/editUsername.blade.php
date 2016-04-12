c6d050e7720e6a9eef584bb24b217d8dbfddfe3d
sihx <telescope72@hotmail.com>
2016-04-12 15:12:28 -0400

    Fixed editUsername

--- laravel/resources/views/editUsername.blade.php
+++ laravel/resources/views/editUsername.blade.php
@@ -2,6 +2,10 @@
 require_once('../mysqli_connect.php');
 $old=$_POST['old'];
 $new=$_POST['new'];
+$newPassword=$_POST['password'];
+$saltValue = createSaltData($new);
+$encryptedPassword = createHashedValue($saltValue, $newPassword);
+
 if (!(isset($old))) {
    $old='';
 }
@@ -20,11 +24,23 @@ $response= mysqli_query($dbc,$query);
 //echo 'Error: ' . mysqli_error($dbc);
 
 if(mysqli_num_rows($response) <= 0){
-  $sql = "UPDATE `users` SET `username`= '$new' WHERE username='$old'";
+  $sql = "UPDATE `users` SET `username`= '$new', `password` = '$encryptedPassword' WHERE username='$old'";
  $response= mysqli_query($dbc,$sql); 
-  echo json_encode(array("result"=>"true","username"=>"$new"));             
+  echo json_encode(array("result"=>"true","username"=>"$new", "encryptedPassword" => "$encryptedPassword"));
 } else {
   echo json_encode(array("result"=>"false","error"=>"usernameexistsalready"));  
 }
 
 mysqli_close($dbc);
+
+
+function createSaltData($username){
+    $saltValue = '$2a$14$'.md5(strtolower($username));
+    return $saltValue;
+}
+
+function createHashedValue($salt, $password){
+    $hashedValue = crypt($password, $salt);
+    $hashedValue = substr($hashedValue, 29);
+    return $hashedValue;
+}
