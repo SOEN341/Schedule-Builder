d07dec4778b95ce646db4e4948f20dfd34869992
sihx <telescope72@hotmail.com>
2016-04-12 01:34:35 -0400

    Updated fetching

--- laravel/resources/views/test.blade.php
+++ laravel/resources/views/test.blade.php
@@ -1,6 +1,7 @@
 <?php
 require_once('../mysqli_connect.php');
 require_once('../preferences');
+require_once('../Course.php');
 //$username=$_POST['username'];
 $username='Jason123';
 
@@ -13,8 +14,9 @@ $array= mysqli_fetch_array($response);
 
 $coursesDone=Array();
 $coursesRem=Array();
-$coursesInformationDone = Array();
-$coursesInformationRem = Array();
+$coursesLectures = Array();
+$coursesTutorials = Array();
+$coursesLabs = Array();
 
 
 //$priorityPrereq = array("COMP 248", "COMP 249", "COMP 352", "COMP 232", "MATH 203", "MATH 204", "COMP 346", "ENCS 282", "SOEN 341", "ENGR 213", "ENGR 233", "MATH 205", "CHEM 205", "CHEM 206", "SOEN 228", "MATH 203", "MATH 204","CHEM 204", "PHYS 204", "PHYS 205", "PHYS 206");
@@ -51,15 +53,78 @@ $courses=  $schedule->courses;
 
 foreach ($courses as $key => $value){
 	//$temp = $value['number'];
-	$queryForCoursesDone = mysqli_fetch_array(mysqli_query($dbc,"SELECT * FROM sections WHERE courseCode = '$value' "));
-	$query ="SELECT * FROM sections where courseCode='$value' ";
-	array_push($coursesInformationDone, $queryForCoursesDone);
+	//$queryForCourseLecture = mysqli_fetch_array(mysqli_query($dbc,"SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Lecture'"));
+	//$queryForCourseTutorial = mysqli_fetch_array(mysqli_query($dbc,"SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Tutorial'"));
+	//$queryForCourseLab = mysqli_fetch_array(mysqli_query($dbc,"SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Lab'"));
 
-	//this returns all the sections
-	//create an object $course containing all the sections(lecture, lab, tutorial) for all the courses in $courses
+	$queryForCourseLecture =mysqli_fetch_assoc(mysqli_query($dbc, "SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Lecture'"));
+	$queryForCourseTutorial = mysqli_fetch_assoc(mysqli_query($dbc, "SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Tutorial'"));
+	$queryForCourseLab = mysqli_fetch_assoc(mysqli_query($dbc, "SELECT * FROM sections WHERE courseCode = '$value' AND type = 'Lab'"));
+	$jsonLectures = json_encode($queryForCourseLecture);
+	$jsonTutorials = json_encode($queryForCourseTutorial);
+	$jsonLabs = json_encode($queryForCourseLab);
+	//echo '"';
+	//$query ="SELECT * FROM sections where courseCode='$value' ";
+	array_push($coursesLectures, $jsonLectures);
+	array_push($coursesTutorials,$jsonTutorials);
+	array_push($coursesLabs, $jsonLabs);
+
+	//TODO: this returns all the sections
+	//TODO: create an object $course containing all the sections(lecture, lab, tutorial) for all the courses in $courses
+	$course = new Course($value, $coursesLectures, $coursesTutorials, $coursesLabs);
+	//var_dump($course->getName());
+	var_dump($course);
 }
 
-print_r(array_values($coursesInformationDone));
+/*
+//print_r(array_values($coursesInformation));
+//var_dump($coursesInformation['0']);
+echo "<br/>";
+var_dump($coursesLectures['0']);
+echo "<br/>";
+var_dump($coursesTutorials['0']);
+echo "<br/>";
+var_dump($coursesLabs['0']);
+echo "<br/>";
+var_dump($coursesLectures['2']);
+echo "<br/>";
+var_dump($coursesTutorials['2']);
+echo "<br/>";
+var_dump($coursesLabs['2']);
+echo "<br/>";
+var_dump($coursesLectures['3']);
+echo "<br/>";
+var_dump($coursesTutorials['3']);
+echo "<br/>";
+var_dump($coursesLabs['3']);
+echo "<br/>";
+var_dump($coursesLectures['4']);
+echo "<br/>";
+var_dump($coursesTutorials['4']);
+echo "<br/>";
+var_dump($coursesLabs['4']);
+echo "<br/>";
+var_dump($coursesLectures['5']);
+echo "<br/>";
+var_dump($coursesTutorials['5']);
+echo "<br/>";
+var_dump($coursesLabs['5']);
+echo "<br/>";
+var_dump($coursesLectures['6']);
+echo "<br/>";
+var_dump($coursesTutorials['6']);
+echo "<br/>";
+var_dump($coursesLabs['6']);
+*/
+/*
+var_dump($coursesLectures);
+echo "<br/>";
+var_dump($coursesTutorials);
+echo "<br/>";
+var_dump($coursesLabs);
+*/
+
+//echo $coursesInformation['0'];
 //print_r(array_values($courses));
 
 //loop through $arrayofcourses
