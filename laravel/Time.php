f3d513bb7901f0b874f43b2f558e804691b1ee48
Gabriele Bavaro <gabriele.bavaro@bell.net>
2016-04-13 01:33:04 -0400

    Fixing up the preferences page
    Modifying the algorithm for the preferences, page, streamlining it etc.

--- laravel/Time.php
+++ laravel/Time.php
@@ -3,11 +3,13 @@ class Time {
     public $Day='';
     public $StartTime='';
     public $EndTime='';
+    public $SectionName='';
 
-    function __construct($OldDay,$OldStart,$OldEnd){
+    function __construct($OldDay,$OldStart,$OldEnd, $Name){
         $this->Day=$OldDay;
         $this->StartTime=$OldStart;
         $this->EndTime=$OldEnd;
+        $this->SectionName=$Name;
     }
 
     function CompareTo($NewDay,$NewStart,$NewEnd){
